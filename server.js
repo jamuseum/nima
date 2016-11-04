0e7ca57c9da88a8ad67e5462c7573a7a1d1bf9ae
Vijay Menon <vijaymenonx@gmail.com>
2016-11-04 12:05:56 -0700

    Set up local-dev server

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const engine = require('ejs-mate')
 
+app.use(express.static('public'))
 
-app.use(express.static(__dirname + '/public')
 app.engine('ejs', engine)
 app.set('view engine', 'ejs')
+
+const indexRoutes = require('./routes/index')
+app.use(indexRoutes)
 
 app.listen(3000, function(err){
 	if(err) console.log(err);
