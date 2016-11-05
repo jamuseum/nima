6c8c4ce9c53bd24591639679ad615ce1a01db001
Vijay <vijaymenonx@gmail.com>
2016-11-04 18:57:28 -0700

    Few more routes

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ app.engine('ejs', engine)
 app.set('view engine', 'ejs')
 
 const indexRoutes = require('./routes/index')
+const userRoutes = require('./routes/community')
+const searchRoutes = require('./routes/search')
+
 app.use(indexRoutes)
+app.use('/user', express.static(__dirname + '/public'), userRoutes)
+app.use('/search', express.static(__dirname + '/public'), searchRoutes)
+
 
 app.listen(port, function(err){
 	if(err) console.log(err);
