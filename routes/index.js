b3ca0990c5e1cb9fe6d5209c509dd5121f69343d
Vijay <vijaymenonx@gmail.com>
2016-11-26 08:20:14 -0800

    Add more fake data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-
+const users = require('../public/test/users')
 
 router.get('/', function(req, res, next){
-	res.render('main/index')
+	res.render('main/index', {
+		users: users
+	})
 })
 
 module.exports = router;
