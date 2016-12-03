3763bcaf0c69eb3ff5e3cf0a02fff13b238f888d
Vijay <vijaymenonx@gmail.com>
2016-12-03 06:01:40 -0800

    Clean up user page, show home and current location

--- public/static/js/script.js
+++ public/static/js/script.js
@@ -1,46 +1,4 @@
-function initMap() {
 
-	var map = new google.maps.Map(document.getElementById('map'), {
-	  zoom: 2,
-	  // scrollwheel:false,
-	  // zoomControl:false,
-	  scaleControl:false,
-	  streetViewControl:false,
-	  mapTypeControl:false,
-	  // draggable:false,
-	  center: {lat:32.89, lng:-97.04}
-	});
-
-	$.get('/test/users.json', function(data){
-
-		for(var i=0; i<data.length; i++)(function(i){
-			var icon = data[i].profile.picture.thumbnail;
-			var user = data[i].profile.firstname;
-
-			
-
-			var geocoder =  new google.maps.Geocoder();
-    		geocoder.geocode( { 'address': data[i].profile.current_city}, function(results, status) {
-	          if (status == google.maps.GeocoderStatus.OK) {
-	            console.log("location : " + results[0].geometry.location.lat() + " " +results[0].geometry.location.lng()); 
-	           
-	            var city = {lat:results[0].geometry.location.lat(), lng:results[0].geometry.location.lng() };
-				console.log(user, icon)
-				var marker = new google.maps.Marker({
-		            position: city,
-		            icon: icon,
-		            map: map,
-		            content:"<div class='map-icon'></div>"
-		        });
-
-	          } else {
-	            console.log("Something got wrong " + status);
-	          }
-	        });
-		})(i);	
-		
-	});
-}
 
 
 $("#activity-all").click(function(e){
