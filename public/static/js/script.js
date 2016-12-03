a4fe9e6bb8dc6afff6e547a9e3c82deea6b40567
Vijay <vijaymenonx@gmail.com>
2016-12-03 03:44:31 -0800

    Fixes for images, cleaner, map is also json wired

--- public/static/js/script.js
+++ public/static/js/script.js
@@ -1,53 +1,49 @@
 function initMap() {
-	var city = {lat: 34.05, lng: -118.24};
-	var city2 = {lat: 33.95, lng: -117.396};
-	var city3 = {lat: 33.79, lng: -118.18};
-	var city4 = {lat: 35.68, lng: 139.67};
+
 	var map = new google.maps.Map(document.getElementById('map'), {
-	  zoom: 10,
+	  zoom: 8,
 	  // scrollwheel:false,
 	  // zoomControl:false,
 	  scaleControl:false,
 	  streetViewControl:false,
 	  mapTypeControl:false,
 	  // draggable:false,
-	  center: city
+	  center: {lat:34.05, lng:-118.24}
 	});
 
-	 var marker = new google.maps.Marker({
-            position: city,
-            icon: 'media/cache/susan-tiny.png',
-            map: map,
-            content:"<div class='map-icon'></div>"
-          });
-
-	 var marker2 = new google.maps.Marker({
-            position: city2,
-            icon: 'media/cache/omar-tiny.png',
-            map: map,
-            content:"<div class='map-icon'></div>"
-          });
-
-	  var marker3 = new google.maps.Marker({
-            position: city3,
-            icon: 'media/cache/edward-tiny.png',
-            map: map,
-            content:"<div class='map-icon'></div>"
-          });
-
-	   var marker4 = new google.maps.Marker({
-            position: city4,
-            icon: 'media/cache/yamada-tiny.png',
-            map: map,
-            content:"<div class='map-icon'></div>"
-          });
-}
-
-$(function(){
 	$.get('/test/users.json', function(data){
-		console.log(data[0].profile.picture.regular);
-	})
-})
+
+		for(var i=0; i<data.length; i++)(function(i){
+			var icon = data[i].profile.picture.thumbnail;
+			var user = data[i].profile.firstname;
+
+			
+
+			var geocoder =  new google.maps.Geocoder();
+    		geocoder.geocode( { 'address': data[i].profile.current_city}, function(results, status) {
+	          if (status == google.maps.GeocoderStatus.OK) {
+	            console.log("location : " + results[0].geometry.location.lat() + " " +results[0].geometry.location.lng()); 
+	           
+	            var city = {lat:results[0].geometry.location.lat(), lng:results[0].geometry.location.lng() };
+				console.log(user, icon)
+				var marker = new google.maps.Marker({
+		            position: city,
+		            icon: icon,
+		            map: map,
+		            content:"<div class='map-icon'></div>"
+		        });
+
+	          } else {
+	            console.log("Something got wrong " + status);
+	          }
+	        });
+		})(i);
+
+			
+		
+	});
+
+}
 
 
 $("#activity-all, #load-more").click(function(e){
